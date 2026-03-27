71123d44794fb3a05e558c015096d5b89cdfa3ef
Yohan Curbelo <ycurbelo@quside.com>
2026-03-27 16:06:55 +0100

    installing cocotb-ext in workflow. jsons updated

--- axil_register/tb/test_axil_register.py
+++ axil_register/tb/test_axil_register.py
@@ -8,18 +8,12 @@ sources_list = glob.glob(dir+"/../src/*.vhd")
 sources_list += glob.glob(dir+"/*.vhd")
 
 @pytest.mark.skipif(os.getenv("SIM") != "ghdl", reason="")
-@pytest.mark.parametrize("parameters", [
-                                        {"AXI_DATA_WIDTH":"32","AXI_ADDR_WIDTH":"32", "REG_TYPE": "0"},
-                                        {"AXI_DATA_WIDTH":"32","AXI_ADDR_WIDTH":"32", "REG_TYPE": "1"},
-                            ])
-def test_axil_register_tb_ghdl(parameters):
+def test_axil_register_tb_ghdl():
     run(
         vhdl_sources=[os.path.join(dir,file)
             for file in sources_list],          # sources
         toplevel="axil_register_top",           # top level HDL
         module="axil_register_tb",              # name of cocotb test module
         toplevel_lang="vhdl",
-        compile_args=["--ieee=synopsys","--std=08"],
-        parameters=parameters,
-        extra_env=parameters 
+        compile_args=["--ieee=synopsys","--std=08"]
     )
